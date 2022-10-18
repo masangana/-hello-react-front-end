56a6ea3e254ec8b6c3b1ede48ffb54e4f55db037
masangana <alexmasangana@gmail.com>
2022-10-19 02:27:45 +0300

    update greeting

--- src/component/greetings.js
+++ src/component/greetings.js
@@ -1,43 +1,38 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMessages } from './redux/messageSlice';
 
-export const Greetings = () => {
-    const message = useSelector((state) => state.message);
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchMessages());
-    }, [dispatch]);
+export const Greeting = () => {
+  const message = useSelector((state) => state.message);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMessages());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return (
+  return (
+    <div>
+      {message.loading && <div>Loading...</div>}
+      {!message.loading && message.error ? (
         <div>
-            {message.loading ? (
-                <h2>Loading...</h2>
-            ) : message.error ? (
-                <h2>{message.error}</h2>
-            ) : (
-                
-                <div>
-
-                    {  
-                        
-
-                        Object.keys(message.messages.data).map((key) => {
-                            return (
-                                <div key={key}>
-                                    <h2>{message.messages.data[key].greeting}</h2>
-                                </div>
-                            );
-                        })
-
-                    }
-                       
-                    
-                </div>
-            )} 
-
+          Error:
+          {message.error}
         </div>
-    );
-}
+      ) : null}
+      {!message.loading && message.messages.length ? (
+        <ul>
+            
+          {message.messages.map((message) => (
+            <li key={message.greeting}>
+              <span>
+                {message.greeting}
+              </span>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+};
 
-export default Greetings;
+export default Greeting;
